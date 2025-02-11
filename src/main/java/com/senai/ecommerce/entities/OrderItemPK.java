180bfe5f008eb528b83b83fedf4a000e4341a9c5
TIAGO LORENZI MATUCHAKI <tiago_matuchaki@estudante.sesisenai.org.br>
2025-02-11 20:53:36 -0300

    ok

--- src/main/java/com/senai/ecommerce/entities/OrderItemPK.java
+++ src/main/java/com/senai/ecommerce/entities/OrderItemPK.java
@@ -1,8 +1,17 @@
 package com.senai.ecommerce.entities;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Embeddable
 public class OrderItemPK {
-//    @Column(name = "order_id")
+   @ManyToOne
+   @JoinColumn(name = "product_id")
+   private Product product;
+
+   @ManyToOne
+   @JoinColumn(name = "order_id")
+   private Order order;
+
 }
